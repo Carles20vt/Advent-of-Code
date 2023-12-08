db48bb3e9fb22577bee3228cdd7d67c9e6d2da0c
Carles Arasil Albert <carles.arasil@gmail.com>
2023-12-08 12:19:22 +0100

    - Added Day 08 with Part 1 done. - TODO Part 2

---
+++ y.2023/Day08/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace Day08;
+
+internal abstract class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            /*
+            if (args.Length < 1)
+            {
+                Console.WriteLine("No input file provided. Please provide one.");
+                return;
+            }
+
+            var filePath = args[0];
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"File not found at path: {filePath}");
+                return;
+            }
+            */
+
+            var filePath = "./Input.txt";
+            //filePath = "./InputExample.txt";
+        
+            var stopwatch = new Stopwatch();
+
+            var mapLines = File.ReadAllLines(filePath);
+
+            stopwatch.Start();
+            var hauntedWasteland = new HauntedWasteland(mapLines);
+            var totalSteps = hauntedWasteland.GetStepsToSoEscape();
+            stopwatch.Stop();
+            
+            Console.WriteLine($"\n[Part 1] - Required {totalSteps} Steps to reach ZZZ - {stopwatch.ElapsedMilliseconds}ms.\n"); 
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
