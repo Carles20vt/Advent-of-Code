3290bd344a31366fd482ce112d01711919a66360
Carles Arasil Albert <carles.arasil@gmail.com>
2023-12-16 22:28:37 +0100

    - Day 17 project creation - Pending to do Part 1 and Part 2

---
+++ y.2023/Day17/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace Day17;
+
+internal abstract class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+        if (args.Length < 1)
+        {
+            Console.WriteLine("No input file provided. Please provide one.");
+            return;
+        }
+
+        var filePath = args[0];
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine($"File not found at path: {filePath}");
+            return;
+        }
+        */
+
+        var filePath = "./Input.txt";
+        filePath = "./InputExample.txt";
+        
+        var stopwatch = new Stopwatch();
+
+        var allLines = File.ReadAllLines(filePath);
+        
+        stopwatch.Start();
+        //var lensLibrary = new TheFloorWillBeLava(allLines.ToList());
+        //var energizedTiles = lensLibrary.CountEnergizedTiles();
+        stopwatch.Stop();
+        //Console.WriteLine($"\n[Part 1] - The tiles amount being energized are: {energizedTiles} - {stopwatch.ElapsedMilliseconds}ms.\n");
+    }
+}
